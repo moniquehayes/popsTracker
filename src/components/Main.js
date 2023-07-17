17a934714f6ac5d3f7487512914ab70981504e47
Monique Hayes <moniqueehayes5@gmail.com>
2023-07-17 10:15:37 -0500

    src folder setup

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+//import components//
+
+// entry point for routes //
+const Main = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Main } from './components';
+
+import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                <Main />
+            </Router>
+        </Provider>
+    </React.StrictMode>
+)
