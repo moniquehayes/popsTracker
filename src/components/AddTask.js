2bd459778ee4e306014c7e869d5c373f7731512e
Monique Hayes <moniqueehayes5@gmail.com>
2023-08-01 11:48:18 -0500

    state and submit function added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,30 @@
 import React, {useState} from 'react';
 import { useDispatch } from 'react-redux';
-// will need to make the api route, redux request and import thunk here 
+import { addTaskAsync } from '../store/taskSlice';
 
 const AddTask = () => {
+    const [type, setType] = useState('');
+    const [mealtype, setMealtype] = useState(null);
+    const [date, setDate] = useState('');
+    const [description, setDescription] = useState(null);
 
+    const dispatch = useDispatch();
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        await dispatch(addTaskAsync({type, mealtype, date, description}));
+        setType('');
+        setMealtype(null);
+        setDate('');
+        setDescription(null);
+    }
+
+    return (
+        // form to add //
+        <div>
+            
+        </div>
+    )
 }
 
 export default AddTask;
