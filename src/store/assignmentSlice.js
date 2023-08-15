3d18fb251b2caf545abed0ad7bc78dff0a0c050b
Monique Hayes <moniqueehayes5@gmail.com>
2023-08-15 15:30:31 -0500

    store and slice created for assignments

---
+++ src/store/assignmentSlice.js
@@ -0,0 +1,25 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchAssignmentsAsync = createAsyncThunk("assignments", async () => {
+    try {
+    const {data} = await axios.get("api/assignments");
+    return data;
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+export const assignmentsSlice = createSlice({
+    name: "assignments",
+    initialState: [],
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchAssignmentsAsync.fulfilled, (state, action) => {
+            return action.payload;
+        })
+    }
+})
+
+export const selectAssignments = (state) => state.assignments;
+
+export default assignmentsSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import caretakersReducer from "./caretakerSlice";
 import taskReducer from "./taskSlice";
+import assignmentReducer from "./assignmentSlice";
 //import reducers // 
 
 const store = configureStore({
     reducer: {
         caretakers: caretakersReducer,
-        tasks: taskReducer
+        tasks: taskReducer,
+        assignments: assignmentReducer
     }
 });
 
